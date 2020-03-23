784a6d5ce133b73dc869d59b1687056d164956f3
ikarian7 <scott.marchant7@gmail.com>
2020-03-23 16:18:50 +0100

    Screen switch working

---
+++ Studentportal/app/src/main/java/com/example/studentportal/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.studentportal
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fabAdd.setOnClickListener {
+            startAddPortalActivity()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun startAddPortalActivity() {
+        val intent = Intent(this, AddPortalActivity::class.java)
+        startActivity(intent)
+    }
+
+}
