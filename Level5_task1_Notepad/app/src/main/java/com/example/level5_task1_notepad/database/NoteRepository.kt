1bd0465d90a0d046b0ff288ba72ffbbfb2157cb4
ikarian7 <scott.marchant7@gmail.com>
2020-05-21 11:45:00 +0200

    step 2 and 3

---
+++ Level5_task1_Notepad/app/src/main/java/com/example/level5_task1_notepad/database/NoteRepository.kt
@@ -0,0 +1,23 @@
+package com.example.level5_task1_notepad.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.example.level5_task1_notepad.model.Note
+
+class NoteRepo(context: Context){
+    private val noteDao: NoteDao
+
+    init {
+        val database = NotepadRoomDatabase.getDatabase((context))
+        noteDao = database!!.noteDao()
+    }
+
+    fun getNotepad(): LiveData<Note?> {
+        return noteDao.getNotepad()
+    }
+
+    suspend fun updateNotepad(note: Note){
+        noteDao.updateNote(note)
+    }
+
+}
